d1c2f75bfbd6e8de2ddea41f891ca66f89bdbf98
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-02-15 20:36:16 +0100

    first version

---
+++ README.md
@@ -0,0 +1,10 @@
+# tensorflow-build
+
+A set of tools to build the tensorflow c lib for various architectures / OS
+
+
+## Requirements
+-`bazel` version `0.4.3` /!\ the `0.4.4` will not work
+- if you want to crosscompile for arm, you also need and arm cross toolchain
+
+
---
+++ cross-compile/crosscompile.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+PATCH_ORIG=tf-crosscompile.patch
+BUILD_DIR=target
+TENSORFLOW_VERSION="v1.0.0-rc2"
+
+CT_DIR=$1
+CT_NAME=$2
+
+GCC=$1/bin/$2-gcc
+GCC_VERSION=$($GCC -dumpversion)
+if [ ! -f $GCC ];then
+	echo "usage: $0 <absolute cross toolchain path> <toolchain prefix>"
+	echo "seaching gcc here : $GCC"
+	exit
+fi 
+
+GCC_VERSION=$($GCC -dumpversion)
+echo "using gcc : $GCC version $GCC_VERSION" 
+
+PATCH_NAME=tf-crosscompile-$CT_NAME.patch
+mkdir $BUILD_DIR
+
+cp $PATCH_ORIG $BUILD_DIR/$PATCH_NAME
+
+cd $BUILD_DIR
+
+sed -i "s#%%CT_NAME%%#$CT_NAME#g" $PATCH_NAME
+sed -i "s#%%CT_ROOT_DIR%%#$CT_DIR#g" $PATCH_NAME
+sed -i "s#%%CT_GCC_VERSION%%#$GCC_VERSION#g" $PATCH_NAME
+
+git clone https://github.com/tensorflow/tensorflow.git
+
+cd tensorflow
+
+git checkout $TENSORFLOW_VERSION || exit 1
+
+git apply ../$PATCH_NAME || exit 1
+
+grep -Rl 'lib64' | xargs sed -i 's/lib64/lib/g'
+
+yes ''|./configure || exit 1
+
+bazel build  -c opt --copt="-mfpu=neon-vfpv4" --copt="-funsafe-math-optimizations" --copt="-ftree-vectorize" --copt="-fomit-frame-pointer"   tensorflow:libtensorflow_c.so --cpu=armeabi-v7a --crosstool_top=//tools/arm_compiler:toolchain --verbose_failures
