702d283abd5e45a109df1bd68cb55cd8477d4371
Mathieu Poumeyrol <kali@zoy.org>
2017-02-23 14:29:08 +0100

    linting the definition

--- Formula/tensorflow_c.rb
+++ Formula/tensorflow_c.rb
@@ -1,11 +1,8 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-
 class TensorflowC < Formula
-  desc "An open-source software library for Machine Intelligence"
+  desc "C interface for Google's OS library for Machine Intelligence"
   homepage "https://www.tensorflow.org/"
-  url "https://github.com/tensorflow/tensorflow", :tag  => "v1.0.0", :using => :git
-  version "1.0.0"
+  url "https://github.com/tensorflow/tensorflow", :using => :git,
+    :tag => "v1.0.0", :revision => "07bb8ea2379bd459832b23951fb20ec47f3fdbd4"
 
   depends_on "bazel" => :build
   depends_on "pkg-config" => :run
@@ -14,7 +11,7 @@ class TensorflowC < Formula
     system 'echo "\n\n\n\n\n\n\n\n\n" | ./configure'
     system "bazel", "build", "--compilation_mode=opt", "--copt=-march=native", "tensorflow:libtensorflow_c.so"
     lib.install "bazel-bin/tensorflow/libtensorflow_c.so"
-    system "cp", "tensorflow/c/c_api.h", "tensorflow_c_api.h"
+    cp "tensorflow/c/c_api.h", "tensorflow_c_api.h"
     include.install "tensorflow_c_api.h"
     pc = <<-EOF.gsub(/^\s+/, "")
       Name: tensorflow_c
@@ -23,13 +20,13 @@ class TensorflowC < Formula
       Libs: -L#{lib} -ltensorflow_c
       Cflags: -I#{include}
     EOF
-    FileUtils.mkdir_p(lib/"pkgconfig")
-    File.open(lib/"pkgconfig/tensorflow_c.pc", 'w') { |f| f.write(pc) }
+    mkdir_p(lib/"pkgconfig")
+    File.open(lib/"pkgconfig/tensorflow_c.pc", "w") { |f| f.write(pc) }
   end
 
   test do
     # test a call on TF_Version(), checking .h, .so, pkg-config setup.
-    File.open("test.c", 'w') { |f|
+    File.open("test.c", "w") do |f|
       f.write(<<-EOF.gsub(/^\s+/, ""))
       #include <stdio.h>
       #include <tensorflow_c_api.h>
@@ -37,7 +34,7 @@ class TensorflowC < Formula
         printf("%s\\n", TF_Version());
       }
       EOF
-    }
+    end
     system "sh", "-c", "gcc `pkg-config --libs --cflags tensorflow_c` -o test_tf test.c"
     found_version = `./test_tf`.strip
     assert found_version == version
