05934c612a31547dd757f8dbb925b6cda86cdf52
Mathieu Poumeyrol <kali@zoy.org>
2017-02-23 13:26:00 +0100

    build native, plus metafields

--- Formula/tensorflow_c.rb
+++ Formula/tensorflow_c.rb
@@ -2,18 +2,17 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 
 class TensorflowC < Formula
-  desc ""
-  homepage ""
+  desc "An open-source software library for Machine Intelligence"
+  homepage "https://www.tensorflow.org/"
   url "https://github.com/tensorflow/tensorflow", :tag  => "v1.0.0", :using => :git
   version "1.0.0"
-  sha256 ""
 
   depends_on "bazel" => :build
   depends_on "pkg-config" => :run
 
   def install
     system 'echo "\n\n\n\n\n\n\n\n\n" | ./configure'
-    system "bazel", "build", "--compilation_mode=opt", "tensorflow:libtensorflow_c.so"
+    system "bazel", "build", "--compilation_mode=opt", "--copt=-march=native", "tensorflow:libtensorflow_c.so"
     lib.install "bazel-bin/tensorflow/libtensorflow_c.so"
     system "cp", "tensorflow/c/c_api.h", "tensorflow_c_api.h"
     include.install "tensorflow_c_api.h"
@@ -29,15 +28,7 @@ class TensorflowC < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test tensorflow_c`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    # test a call on TF_Version(), checking .h, .so, pkg-config setup.
     File.open("test.c", 'w') { |f|
       f.write(<<-EOF.gsub(/^\s+/, ""))
       #include <stdio.h>
