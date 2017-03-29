3eafca91ffaaf4cc7d0c22c25bb9e55e760f67cb
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-03-29 18:09:31 +0200

    install c_api.h in debs see #9

--- debian/create-deb-native.sh
+++ debian/create-deb-native.sh
@@ -1,10 +1,11 @@
 #/usr/bin/env bash
 
 TARGET_SO_FILE=target/tensorflow/bazel-bin/tensorflow/libtensorflow.so
+C_HEADERS_FILE=target/tensorflow/tensorflow/c/c_api.h
 
 . ./versions.sh
 
 ./compile.sh "$TF_VERSION"
-./create-deb.sh "$TARGET_SO_FILE" amd64 "$TF_DEB_VERSION"
+./create-deb.sh "$TARGET_SO_FILE" "$C_HEADERS_FILE" amd64 "$TF_DEB_VERSION"
 
 
--- debian/create-deb.sh
+++ debian/create-deb.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 SO_FILE=$1
-SO_ARCH=$2
-SO_VERSION=$3
+HEADERS_FILE=$2
+SO_ARCH=$3
+SO_VERSION=$4
 
 BUILD_DIR=target
 
@@ -15,13 +16,19 @@ CONTROL_FILE=control
 
 SO_INSTALL_DIR="/usr/lib/"
 PC_INSTALL_DIR="/usr/lib/pkgconfig/"
+HEADERS_INSTALL_DIR="/usr/include/$LIB_NAME"
 
 SO_INSTALL_NAME="lib$LIB_NAME.so"
 PC_INSTALL_NAME="$LIB_NAME.pc"
+HEADERS_INSTALL_NAME="c_api.h"
 
 if [ -z $SO_FILE ] || [ ! -f $SO_FILE ]
 then
 	echo "file not found $SO_FILE"
+fi
+if [ -z $HEADERS_FILE ] || [ ! -f $HEADERS_FILE ]
+then
+	echo "file not found $HEADERS_FILE"
 fi
 if [ -z $SO_ARCH ]
 then
@@ -31,9 +38,9 @@ if [ -z $SO_VERSION ]
 then
 	echo "version not found"
 fi
-if [ -z $SO_FILE ] || [ ! -f $SO_FILE ] || [ -z $SO_ARCH ] || [ -z $SO_VERSION ]
+if [ -z $SO_FILE ] || [ ! -f $SO_FILE ] || [ -z $HEADERS_FILE ] || [ ! -f $HEADERS_FILE ] || [ -z $SO_ARCH ] || [ -z $SO_VERSION ]
 then
-	echo "usage: $0 <path/to/libtensorflow.so> <arch> <version>"
+	echo "usage: $0 <path/to/libtensorflow.so> <path/to/c/headers> <arch> <version>"
 	exit 1
 fi
 
@@ -41,9 +48,11 @@ DEB_NAME="${PACKAGE_NAME}_${SO_VERSION}_${SO_ARCH}"
 
 mkdir -p "$BUILD_DIR/$DEB_NAME/$SO_INSTALL_DIR"
 mkdir -p "$BUILD_DIR/$DEB_NAME/$PC_INSTALL_DIR"
+mkdir -p "$BUILD_DIR/$DEB_NAME/$HEADERS_INSTALL_DIR"
 mkdir -p "$BUILD_DIR/$DEB_NAME/$DEBIAN_DIR"
 
 cp "$SO_FILE" "$BUILD_DIR/$DEB_NAME/$SO_INSTALL_DIR/$SO_INSTALL_NAME"
+cp "$HEADERS_FILE" "$BUILD_DIR/$DEB_NAME/$HEADERS_INSTALL_DIR/$HEADERS_INSTALL_NAME"
 
 cd $BUILD_DIR
 
