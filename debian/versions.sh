f2343df36b69f193b1f1ccdf669e22faf68c6b3b
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-10-30 11:31:32 +0100

    update tf to version 1.3.1

--- debian/versions.sh
+++ debian/versions.sh
@@ -1,4 +1,4 @@
 #/usr/env/bin bash
-export TF_VERSION="v1.2.1"
-export TF_DEB_VERSION="1.2.1-snips-1"
+export TF_VERSION="107cc777af7880c140d089e44ad898a6ba929286"
+export TF_DEB_VERSION="1.3.1-snips-1"
 
