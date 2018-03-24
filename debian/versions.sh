29842cc3e1a848e505dd6a19f65fb683b6a1d79c
mtrey <marc.trey@snips.ai>
2018-03-24 05:50:05 +0100

    bump to tf 1.6.0

--- debian/versions.sh
+++ debian/versions.sh
@@ -1,4 +1,4 @@
 #/usr/env/bin bash
-export TF_VERSION="107cc777af7880c140d089e44ad898a6ba929286"
-export TF_DEB_VERSION="1.3.1-snips-2"
+export TF_VERSION="d2e24b6039433bd83478da8c8c2d6c58034be607"
+export TF_DEB_VERSION="1.6.0-snips"
 
